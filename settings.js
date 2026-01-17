90dd6098c6c709ecfd23c2831833e63fc2dbc066
Viso Studio <visostudio25@gmail.com>
2026-01-17 02:08:53 +0000

    Update bot settings and metadata

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Viso Studio',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Viso Bot",
+  botOwner: 'Ivan', // Your name
+  ownerNumber: '2250151191073', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "TUne Vision Différente Des Choses.",
   version: "3.0.6",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
